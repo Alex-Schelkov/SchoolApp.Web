28f7d6f6dfbabc8abc3fccd50ec2c0ddab5d31da
Щелков Алексей Иванович <orenburgstyle@mail.ru>
2019-02-25 23:27:15 +0500

    Создан функционал для создания  заявок

--- SchoolApp.Web/Models/ApplicationDbContext.cs
+++ SchoolApp.Web/Models/ApplicationDbContext.cs
@@ -13,7 +13,13 @@ namespace SchoolApp.Web.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
            : base(options)
         {
-            Database.EnsureCreated();
+            if (Database.EnsureCreated())
+            {
+                Database.ExecuteSqlCommand("INSERT INTO `helpdeskschool`.`states` (`Id`,`Name`) VALUES(1,'Создано');");
+                Database.ExecuteSqlCommand("INSERT INTO `helpdeskschool`.`states` (`Id`,`Name`) VALUES(2,'В работе');");
+                Database.ExecuteSqlCommand("INSERT INTO `helpdeskschool`.`states` (`Id`,`Name`) VALUES(3,'Исполнено');");
+                Database.ExecuteSqlCommand("INSERT INTO `helpdeskschool`.`states` (`Id`,`Name`) VALUES(4,'Архив');");
+            }
         }
 
   
@@ -21,7 +27,7 @@ namespace SchoolApp.Web.Models
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
+            
         }
 
         public DbSet<State> States { get; set; }
@@ -51,25 +57,34 @@ namespace SchoolApp.Web.Models
 
         [Key]
         [Display(Name = "Номер")]
+    
         public int Id { get; set; }
-        [Required]
+
+        [Required (ErrorMessage = "Заполните поле 'Название'!")]
         [Display(Name = "Название")]
         public string Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Заполните поле 'Описание проблемы'!")]
         [Display(Name = "Описание проблемы")]
         public string Text { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Заполните поле 'Кабинет'!")]
         [Display(Name = "Кабинет")]
         public string Cabinet { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Заполните поле 'ФИО сотрудника'!")]
         [Display(Name = "ФИО сотрудника")]
         public string Fio { get; set; }
+
         [Display(Name = "Дата создания")]
         public DateTime StartDate { get; set; } = DateTime.Now;
+
         [Display(Name = "Дата принятия в работу")]
         public DateTime? WorkDate { get; set; }
+
         [Display(Name = "Статус")]
         public int? StateId { get; set; } = 1;
+
         public State State { get; set; }
 
     }
